9684d916ce4868cd8984e9003119df243ce32fb4
thorcc <thorchrcow@gmail.com>
2023-03-29 14:39:55 +0200

    eksempel: oppsett av pygame

--- pygame/oppsett.py
+++ pygame/oppsett.py
@@ -7,6 +7,7 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
